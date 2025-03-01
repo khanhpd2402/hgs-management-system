8bba21d9e46a4e2b12f99e3fa5464c186b37d712
Trungdh030503 <trung.030503@gmail.com>
2025-03-02 03:13:11 +0700

    CRUD User

---
+++ HGSMServer/Application/Features/Users/DTOs/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Features.Users.DTOs
+{
+    public class UserDTO
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string? Status { get; set; }
+    }
+}
