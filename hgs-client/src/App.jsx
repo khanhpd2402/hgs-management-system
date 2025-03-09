062e23cdb850168a907261ad33a583de340ee13e
kzau1612 <ngohoangkim2002@gmail.com>
2025-03-09 22:06:33 +0700

    add student profile

--- hgs-client/src/App.jsx
+++ hgs-client/src/App.jsx
@@ -1,7 +1,13 @@
+import StudentProfile from "./pages/Student/Profile/StudentProfile";
+import AddStudentForm from "./pages/Teacher/Profile/AddStudentForm";
 import AppRouter from "./routes/AppRouter";
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    // <AppRouter />
+    // <AddStudentForm />
+    <StudentProfile />
+  );
 };
 
 export default App;
