c262b8f00cf4d49118fa08fc33b07ae97857c202
kzau1612 <ngohoangkim2002@gmail.com>
2025-03-11 17:15:44 +0700

    ađ Student Score

--- hgs-client/src/App.jsx
+++ hgs-client/src/App.jsx
@@ -1,14 +1,18 @@
 import StudentProfile from "./pages/Student/Profile/StudentProfile";
 import AddStudentForm from "./pages/Student/Profile/AddStudentForm";
 import AppRouter from "./routes/AppRouter";
-import SummaryStudentScore from "./pages/Student/SummaryScore/SummaryStudentScore";
+import StudentListScore from "./pages/Student/SummaryScore/StudentListScore";
+import MarkReportTable from "./pages/Teacher/MarkReport/MarkReportTable";
+import StudentScore from "./pages/Student/SummaryScore/StudentScore";
 
 const App = () => {
   return (
     // <AppRouter />
     // <AddStudentForm />
-    // <StudentProfile />
-    <SummaryStudentScore />
+    // // <StudentProfile />
+    // <MarkReportTable />
+    // <StudentListScore />
+    <StudentScore />
   );
 };
 
