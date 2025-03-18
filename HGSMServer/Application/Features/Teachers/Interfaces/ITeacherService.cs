80acdb8e1d09a1e293c104f2f6601c0d125a2cf2
khanhpd2402 <khanhpd2402@gmail.com>
2025-03-18 20:37:03 +0700

    code grades

--- HGSMServer/Application/Features/Teachers/Interfaces/ITeacherService.cs
+++ HGSMServer/Application/Features/Teachers/Interfaces/ITeacherService.cs
@@ -7,7 +7,7 @@ namespace Application.Features.Teachers.Interfaces
 {
     public interface ITeacherService
     {
-        Task<TeacherListResponseDto> GetAllTeachersAsync(bool exportToExcel = false, List<string> selectedColumns = null);
+        Task<List<TeacherListDto>> GetAllTeachersAsync(bool exportToExcel = false, List<string> selectedColumns = null);
         Task<TeacherDetailDto?> GetTeacherByIdAsync(int id); // Phải có dòng này
         Task AddTeacherAsync(TeacherListDto teacherDto);
         Task UpdateTeacherAsync(int id, TeacherDetailDto teacherDto);
