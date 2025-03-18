fd9f61114cf457d61af1614e23a6b2fdd5c49c6e
Trungdh030503 <trung.030503@gmail.com>
2025-03-18 13:55:52 +0700

    assign role, asign homeroom teacher

--- HGSMServer/Application/Features/Teachers/Interfaces/ITeacherService.cs
+++ HGSMServer/Application/Features/Teachers/Interfaces/ITeacherService.cs
@@ -1,18 +1,20 @@
 ﻿using Application.Features.Teachers.DTOs;
 using Microsoft.AspNetCore.Http;
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
+
 namespace Application.Features.Teachers.Interfaces
 {
     public interface ITeacherService
     {
         Task<TeacherListResponseDto> GetAllTeachersAsync(bool exportToExcel = false, List<string> selectedColumns = null);
-        Task<TeacherDetailDto?> GetTeacherByIdAsync(int id); // Phải có dòng này
+        Task<TeacherDetailDto?> GetTeacherByIdAsync(int id);
         Task AddTeacherAsync(TeacherListDto teacherDto);
         Task UpdateTeacherAsync(int id, TeacherDetailDto teacherDto);
         Task DeleteTeacherAsync(int id);
         Task ImportTeachersFromExcelAsync(IFormFile file);
+        Task AssignHomeroomAsync(AssignHomeroomDto assignHomeroomDto);
+        Task<bool> IsHomeroomAssignedAsync(int teacherId, int classId, int academicYearId);
+        Task<bool> HasHomeroomTeacherAsync(int classId, int academicYearId);
     }
-
 }
