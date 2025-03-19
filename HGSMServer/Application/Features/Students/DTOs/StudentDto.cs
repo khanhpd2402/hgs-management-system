62e14d6c916d78fcb399c53614401eab88ceb75e
kzau1612 <ngohoangkim2002@gmail.com>
2025-03-19 23:40:12 +0700

    update

--- HGSMServer/Application/Features/Students/DTOs/StudentDto.cs
+++ HGSMServer/Application/Features/Students/DTOs/StudentDto.cs
@@ -1,5 +1,4 @@
-﻿using Application.Features.Parents.DTOs;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +10,10 @@ namespace Application.Features.Students.DTOs
     {
         public int StudentId { get; set; }
         public string FullName { get; set; } = null!;
-        public DateOnly Dob { get; set; }
+        public DateOnly Dob { get; set; }  // Ngày sinh
         public string Gender { get; set; } = null!;
-        public string ClassName { get; set; } = null!;
-        public int Grade { get; set; }
-        public DateOnly AdmissionDate { get; set; }
+        public int ClassId { get; set; }
+        public DateOnly AdmissionDate { get; set; }  // Ngày nhập học
         public string? EnrollmentType { get; set; }
         public string? Ethnicity { get; set; }
         public string? PermanentAddress { get; set; }
@@ -24,8 +22,5 @@ namespace Application.Features.Students.DTOs
         public bool? RepeatingYear { get; set; }
         public string? IdcardNumber { get; set; }
         public string? Status { get; set; }
-
-        public List<ParentDto> Parents { get; set; } = new();
     }
-
 }
