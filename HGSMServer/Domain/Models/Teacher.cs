55ebff01ae666d26122025220aa92a732aa4353d
Trungdh030503 <trung.030503@gmail.com>
2025-04-04 16:57:47 +0700

    import teacher via excel and edit teacher db

--- HGSMServer/Domain/Models/Teacher.cs
+++ HGSMServer/Domain/Models/Teacher.cs
@@ -31,7 +31,7 @@ public partial class Teacher
 
     public string? Department { get; set; }
 
-    public string? AdditionalDuties { get; set; }
+    public string? MainSubject { get; set; }
 
     public bool? IsHeadOfDepartment { get; set; }
 
