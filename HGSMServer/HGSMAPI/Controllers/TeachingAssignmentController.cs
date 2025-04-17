5ed8a2b07c9f9444028196ab2e1c12add11dd2a6
Trungdh030503 <trung.030503@gmail.com>
2025-04-17 23:33:29 +0700

    fix bug

--- HGSMServer/HGSMAPI/Controllers/TeachingAssignmentController.cs
+++ HGSMServer/HGSMAPI/Controllers/TeachingAssignmentController.cs
@@ -1,5 +1,4 @@
-﻿using Application.Features.HomeRooms.DTOs;
-using Application.Features.TeachingAssignments.DTOs;
+﻿using Application.Features.TeachingAssignments.DTOs;
 using Application.Features.TeachingAssignments.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -92,9 +91,9 @@ namespace HGSMAPI.Controllers
             }
         }
 
-        [HttpGet("by-teacherId&semesterId/{teacherId}")]
+        [HttpGet("teacher/{teacherId}/semester/{semesterId}")]
         [Authorize(Roles = "Hiệu trưởng,Hiệu phó,Trưởng bộ môn,Cán bộ văn thư,Teacher")]
-        public async Task<IActionResult> GetTeachingAssignmentsByTeacherId(int teacherId, [FromQuery] int semesterId)
+        public async Task<IActionResult> GetTeachingAssignmentsByTeacherId(int teacherId, int semesterId)
         {
             try
             {
@@ -106,7 +105,8 @@ namespace HGSMAPI.Controllers
                 return BadRequest(ex.Message);
             }
         }
-        [HttpDelete("by-teacher/{teacherId}/semester/{semesterId}")]
+
+        [HttpDelete("teacher/{teacherId}/semester/{semesterId}")]
         [Authorize(Roles = "Hiệu trưởng,Hiệu phó,Trưởng bộ môn,Cán bộ văn thư")]
         public async Task<IActionResult> DeleteTeachingAssignmentsByTeacherIdAndSemesterId(int teacherId, int semesterId)
         {
