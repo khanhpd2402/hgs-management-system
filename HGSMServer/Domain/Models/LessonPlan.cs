f32e71b2f15f262db8564e15129e13950d456556
khanhpd2402 <khanhpd2402@gmail.com>
2025-04-19 23:19:12 +0700

    fix bugs

--- HGSMServer/Domain/Models/LessonPlan.cs
+++ HGSMServer/Domain/Models/LessonPlan.cs
@@ -29,6 +29,10 @@ public partial class LessonPlan
 
     public int? ReviewerId { get; set; }
 
+    public DateOnly? EndDate { get; set; }
+
+    public DateOnly? StartDate { get; set; }
+
     public virtual Teacher? Reviewer { get; set; }
 
     public virtual Semester Semester { get; set; } = null!;
